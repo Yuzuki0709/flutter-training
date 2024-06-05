fa0eb759c9900301788e51cb0aec7b4c3eb4d815
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2024-06-05 09:27:55 +0900

    feat: WeatherScreenにScaffoldを設定

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: WeatherScreen(),
-      ),
+      home: WeatherScreen(),
     );
   }
 }
--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -5,80 +5,82 @@ class WeatherScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FractionallySizedBox(
-        widthFactor: 0.5,
-        child: Column(
-          children: [
-            const Spacer(),
+    return Scaffold(
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          child: Column(
+            children: [
+              const Spacer(),
 
-            // Placeholder
-            const AspectRatio(
-              aspectRatio: 1,
-              child: Placeholder(),
-            ),
-
-            // Texts
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: DefaultTextStyle(
-                style: Theme.of(context).textTheme.labelLarge!,
-                child: const Row(
-                  children: [
-                    Expanded(
-                      child: Center(
-                        child: Text(
-                          '** ℃',
-                          style: TextStyle(
-                            color: Colors.blue,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Center(
-                        child: Text(
-                          '** ℃',
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+              // Placeholder
+              const AspectRatio(
+                aspectRatio: 1,
+                child: Placeholder(),
               ),
-            ),
 
-            // Buttons
-            Expanded(
-              child: Column(
-                children: [
-                  const SizedBox(height: 80),
-                  Row(
+              // Texts
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: DefaultTextStyle(
+                  style: Theme.of(context).textTheme.labelLarge!,
+                  child: const Row(
                     children: [
                       Expanded(
                         child: Center(
-                          child: TextButton(
-                            onPressed: () {},
-                            child: const Text('Close'),
+                          child: Text(
+                            '** ℃',
+                            style: TextStyle(
+                              color: Colors.blue,
+                            ),
                           ),
                         ),
                       ),
                       Expanded(
                         child: Center(
-                          child: TextButton(
-                            onPressed: () {},
-                            child: const Text('Reload'),
+                          child: Text(
+                            '** ℃',
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
                           ),
                         ),
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+
+              // Buttons
+              Expanded(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 80),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Center(
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text('Close'),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Center(
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text('Reload'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
