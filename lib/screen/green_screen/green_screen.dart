a07067797c6b0dd3c365188b40c326b8ee08483c
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2024-06-06 20:53:14 +0900

    feat: GreenScreen表示の0.5秒後にWeatherScreenに遷移するようにした

--- lib/screen/green_screen/green_screen.dart
+++ lib/screen/green_screen/green_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_training/screen/weather_screen/weather_screen.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
 
 class GreenScreen extends StatefulWidget {
   const GreenScreen({super.key});
@@ -10,6 +12,20 @@ class GreenScreen extends StatefulWidget {
 class _GreenScreenState extends State<GreenScreen> {
   @override
   Widget build(BuildContext context) {
+    Future(() async {
+      await WidgetsBinding.instance.endOfFrame.then((_) {
+        Future.delayed(const Duration(milliseconds: 500), () async {
+          await Navigator.of(context).push(
+            MaterialPageRoute<void>(
+              builder: (_) => WeatherScreen(
+                yumemiWeather: YumemiWeather(),
+              ),
+            ),
+          );
+          setState(() {});
+        });
+      });
+    });
     return const ColoredBox(color: Colors.green);
   }
 }
