b1adedb2de6f1620513353cc34093c0591f00cbe
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2024-06-10 12:20:25 +0900

    feat: initState内で遷移処理を行うようにした

--- lib/screen/green_screen/green_screen.dart
+++ lib/screen/green_screen/green_screen.dart
@@ -11,21 +11,32 @@ class GreenScreen extends StatefulWidget {
 
 class _GreenScreenState extends State<GreenScreen> {
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
     Future(() async {
-      await WidgetsBinding.instance.endOfFrame.then((_) {
-        Future.delayed(const Duration(milliseconds: 500), () async {
-          await Navigator.of(context).push(
-            MaterialPageRoute<void>(
-              builder: (_) => WeatherScreen(
-                yumemiWeather: YumemiWeather(),
-              ),
-            ),
-          );
-          setState(() {});
-        });
-      });
+      await WidgetsBinding.instance.endOfFrame.then((_) => _toWeatherScreen());
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return const ColoredBox(color: Colors.green);
+  }
+
+  Future<void> _toWeatherScreen() async {
+    await Future<void>.delayed(const Duration(milliseconds: 500))
+        .then((_) async {
+      if (!context.mounted) {
+        return;
+      }
+
+      await Navigator.push(
+        context,
+        MaterialPageRoute<void>(
+          builder: (context) => WeatherScreen(yumemiWeather: YumemiWeather()),
+        ),
+      );
+      await _toWeatherScreen();
+    });
   }
 }
