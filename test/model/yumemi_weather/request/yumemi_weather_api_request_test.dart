38dc1f15d013af98f34bf78d08896b948c28bdcc
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2024-06-21 11:01:29 +0900

    feat: request/yumemi_weather_api_request_testにGIVEN-WHEN-THEN構文を適応した

--- test/model/yumemi_weather/request/yumemi_weather_api_request_test.dart
+++ test/model/yumemi_weather/request/yumemi_weather_api_request_test.dart
@@ -2,19 +2,21 @@ import 'package:flutter_training/model/yumemi_weather/request/yumemi_weather_api
 import 'package:test/test.dart';
 
 void main() {
-  group('to json', () {
-    test('success', () {
-      final request = YumemiWeatherApiRequest(
-        area: 'Tokyo',
-        date: DateTime(2024, 6, 20),
-      );
+  group('When toJson is called', () {
+    group('and in case request is valid', () {
+      test('will return valid json', () {
+        final request = YumemiWeatherApiRequest(
+          area: 'Tokyo',
+          date: DateTime(2024, 6, 20),
+        );
 
-      final result = request.toJson();
+        final result = request.toJson();
 
-      expect(
-        result,
-        {'area': 'Tokyo', 'date': '2024-06-20T00:00:00.000'},
-      );
+        expect(
+          result,
+          {'area': 'Tokyo', 'date': '2024-06-20T00:00:00.000'},
+        );
+      });
     });
   });
 }
