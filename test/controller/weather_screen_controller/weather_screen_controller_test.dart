1689dc07418013d104b081f4e388dcd136146d1a
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2024-07-02 15:07:40 +0900

    feat: WeatherScreenControllerのsyncFetchWeatherのテストを追加
    動作を保証するため

--- test/controller/weather_screen_controller/weather_screen_controller_test.dart
+++ test/controller/weather_screen_controller/weather_screen_controller_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_training/data/yumemi_weather_repository.dart';
 import 'package:flutter_training/model/weather_condition.dart';
 import 'package:flutter_training/model/yumemi_weather/request/yumemi_weather_api_request.dart';
 import 'package:flutter_training/model/yumemi_weather/response/yumemi_weather_api_response.dart';
+import 'package:flutter_training/screen/weather_screen/controller/state/weather_screen_controller_state.dart';
 import 'package:flutter_training/screen/weather_screen/controller/weather_screen_controller.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -90,6 +91,83 @@ void main() {
           () => container
               .read(weatherScreenControllerProvider.notifier)
               .fetchWeather(request: request),
+          throwsA(
+            isA<YumemiWeatherError>().having(
+              (error) => error,
+              'invalid parameter',
+              YumemiWeatherError.invalidParameter,
+            ),
+          ),
+        );
+      });
+    });
+  });
+
+  group('When syncFetchWeather is called', () {
+    group('and in case request is valid', () {
+      test('will return YumemiWeatherApiResponse', () async {
+        when(
+          mockRepository.syncFetchWeather(request),
+        ).thenAnswer((_) => Future.value(response));
+
+        final container = createContainer([
+          yumemiWeatherRepositoryProvider.overrideWithValue(mockRepository),
+        ]);
+
+        container.listen(weatherScreenControllerProvider, (_, __) {});
+
+        await container
+            .read(weatherScreenControllerProvider.notifier)
+            .syncFetchWeather(
+              request: request,
+            );
+
+        expect(
+          container.read(weatherScreenControllerProvider),
+          isA<WeatherScreenControllerState>()
+              .having((state) => state.response, 'weather data', response),
+        );
+      });
+      test('will return unkown error', () async {
+        when(
+          mockRepository.syncFetchWeather(request),
+        ).thenThrow(YumemiWeatherError.unknown);
+
+        final container = createContainer([
+          yumemiWeatherRepositoryProvider.overrideWithValue(mockRepository),
+        ]);
+
+        expect(
+          () => container
+              .read(weatherScreenControllerProvider.notifier)
+              .syncFetchWeather(request: request),
+          throwsA(
+            isA<YumemiWeatherError>().having(
+              (error) => error,
+              'error',
+              YumemiWeatherError.unknown,
+            ),
+          ),
+        );
+      });
+    });
+
+    group('and in case requets is invalid', () {
+      test('will return invalid parameter error', () async {
+        when(
+          mockRepository.syncFetchWeather(request),
+        ).thenThrow(
+          YumemiWeatherError.invalidParameter,
+        );
+
+        final container = createContainer([
+          yumemiWeatherRepositoryProvider.overrideWithValue(mockRepository),
+        ]);
+
+        expect(
+          () => container
+              .read(weatherScreenControllerProvider.notifier)
+              .syncFetchWeather(request: request),
           throwsA(
             isA<YumemiWeatherError>().having(
               (error) => error,
